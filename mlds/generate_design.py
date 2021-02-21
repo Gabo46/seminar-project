94fae7c486e639cca54242496f1906594fab603d
Gabriel Dogadov <dogadov.gabriel@gmail.com>
2021-02-21 15:21:21 +0100

    added designs

--- mlds/generate_design.py
+++ mlds/generate_design.py
@@ -29,10 +29,12 @@ import random
 import os
 
 ##############################################################################
+
+
 def shuffle_triad(t):
-    
+
     # randomly chooses if triad is increasing or decreasing
-    if random.randint(0,1)==1:
+    if random.randint(0, 1) == 1:
         t1 = t[0]
         t2 = t[1]
         t3 = t[2]
@@ -40,14 +42,14 @@ def shuffle_triad(t):
         t1 = t[2]
         t2 = t[1]
         t3 = t[0]
-        
+
     return (t1, t2, t3)
 
 
 def shuffle_quadruple(t):
-    
+
     # randomly chooses if triad is increasing or decreasing
-    if random.randint(0,1)==1:
+    if random.randint(0, 1) == 1:
         t1 = t[0]
         t2 = t[1]
         t3 = t[2]
@@ -57,45 +59,45 @@ def shuffle_quadruple(t):
         t2 = t[2]
         t3 = t[1]
         t4 = t[0]
-        
+
     return (t1, t2, t3, t4)
 
 
 def create_design_quadruples(v):
-        
+
     trials = list(itertools.combinations(v, 4))
-    
+
     s1 = []
     s2 = []
     s3 = []
     s4 = []
     for t in trials:
         t1, t2, t3, t4 = shuffle_quadruple(t)
-        
+
         s1.append(t1)
         s2.append(t2)
         s3.append(t3)
         s4.append(t4)
-    
+
     data = {'S1': s1, 'S2': s2, 'S3': s3, 'S4': s4}
 
     return data
 
 
 def create_design_triads(v):
-        
+
     trials = list(itertools.combinations(v, 3))
-    
+
     s1 = []
     s2 = []
     s3 = []
     for t in trials:
         t1, t2, t3 = shuffle_triad(t)
-        
+
         s1.append(t1)
         s2.append(t2)
         s3.append(t3)
-        
+
     data = {'S1': s1, 'S2': s2, 'S3': s3}
 
     return data
@@ -103,55 +105,70 @@ def create_design_triads(v):
 
 ##############################################################################
 ###################### Generting Design Files ################################
-Nrep = 5         # number of repetitions. At least 3, 5 recommended 
-obsname = 'Gabriel' # observer name, in my case, GA.
+Nrep = 5         # number of repetitions. At least 3, 5 recommended
+obsname = 'Muhammed'  # observer name, in my case, GA.
+image = "face"
 
-# vector with all stimuli filenames in each condition. In this example we have 
+# vector with all stimuli filenames in each condition. In this example we have
 # two conditions, 'red' and 'blue', and 6 stimuli per condition
-condA = ['../images/landscape/original.jpg', '../images/landscape/x2_scaled.jpg', '../images/landscape/x4_scaled.jpg', '../images/landscape/x8_scaled.jpg', '../images/landscape/x16_scaled.jpg']
-condB = ['../images/landscape/original.jpg', '../images/landscape/30_scaled.jpg', '../images/landscape/60_scaled.jpg', '../images/landscape/75_scaled.jpg', '../images/landscape/85_scaled.jpg', '../images/landscape/95_scaled.jpg']
+condA = [
+    '../images/%s/original.jpg' % image,
+    '../images/%s/x2_scaled.jpg' % image,
+    '../images/%s/x4_scaled.jpg' % image,
+    '../images/%s/x8_scaled.jpg' % image,
+    '../images/%s/x16_scaled.jpg' % image
+]
+condB = [
+    '../images/%s/original.jpg' % image,
+    '../images/%s/30_scaled.jpg' % image,
+    '../images/%s/60_scaled.jpg' % image,
+    '../images/%s/75_scaled.jpg' % image,
+    '../images/%s/85_scaled.jpg' % image,
+    '../images/%s/95_scaled.jpg' % image
+]
 
-conditions = [condA, condB] # extend this list if necessary
+conditions = [condA, condB]  # extend this list if necessary
 
-exp = 'triads' # 'triads' or 'quadruples'
+exp = 'triads'  # 'triads' or 'quadruples'
 
-### Added line: Create folder for observer if not exsists
+# Added line: Create folder for observer if not exsists
 
-outDir = './designs/%s' % obsname
+outDir = './designs/%s/%s' % (image, obsname)
 
 if not os.path.exists(outDir):
-    os.mkdir(outDir)
+    os.makedirs(outDir)
 
 ###############################################################################
 for rep in range(Nrep):
 
-    if exp=='triads':    
+    if exp == 'triads':
         trials = [create_design_triads(cond) for cond in conditions]
-        
-    elif exp=='quadruples':
+
+    elif exp == 'quadruples':
         trials = [create_design_quadruples(cond) for cond in conditions]
 
     else:
         raise Exception('type of experiment not recognized')
-    
+
     # creates pandas Dataframe for all trials in each condition
     dd = []
     for i, cond in enumerate(conditions):
         d = pd.DataFrame(trials[i])
-        dd.append(d) 
-    
+        dd.append(d)
+
     # joins the conditions
     df = pd.concat(dd)
-        
+
     # shuffles order
     df.reset_index(drop=True, inplace=True)
     df = df.reindex(np.random.permutation(df.index))
     df.reset_index(drop=True, inplace=True)
-    
+
     df.index.name = 'Trial'
-    
+
     # save in design folder, under block number
-    df.to_csv('designs/%s/%s_%s_%d.csv' % (obsname, obsname, exp, rep), index=False)
-    
+    df.to_csv('designs/%s/%s/%s_%d.csv' %
+              (image, obsname, exp, rep), index=False)
+
 
 ##############################################################################
